e209a996c7ac5b76b05d3e8ac6bbb82a5f23368c
jmukes97 <jmukes@oakland.edu>
2019-02-05 16:58:36 -0500

    implimented the method ninty_nine_bottles

--- BeerSong.java
+++ BeerSong.java
@@ -1,6 +1,25 @@
 public class BeerSong{
-	public static void main(){
+	public static void main(String[] args){
+		Ninety_Nine_Bottles_of_Beer();
 	}
-	public void Ninety_Nine_Bottles_of_Beer(){
+	public static void Ninety_Nine_Bottles_of_Beer(){
+		int i = 99;
+		while(i > 0){
+			System.out.println( i + "  bottles of beer on the wall, " + i + " bottles of beer");
+			i--;
+			if(i == 0){
+				System.out.println("Take one down and pass it around, no more bottles of beer on the wall.\n\n");
+			}else{
+				System.out.println("takeone down, pass it around, " + i + " bottles of beer on the wall.\n\n");
+			}
+			try{
+				Thread.sleep(1000);	
+			}
+			catch(InterruptedException e){
+				System.out.println(e);
+			}
+		}
+		System.out.println("No more bottles of beer on the wall, no more bottles of beer.");
+		System.out.println("Go to the store and buy some more, 99 bottles of beer on the wall.\n\n\n");
 	}
 }
